229ba55f11e5552a6aa1a311214443100345c64f
liandro98 <miguelalejandroespinosa7@gmail.com>
2024-11-23 22:21:23 -0600

    Generacion del servicio de productos

--- src/app/pages/admin/productos/productos.component.ts
+++ src/app/pages/admin/productos/productos.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnDestroy, OnInit, ViewChild, Input } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-productos',
